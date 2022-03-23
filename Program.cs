2e260b16ecc63ab7aaca3b8805d382261d967f6c
ALLUK <lam83@bk.ru>
2022-03-24 00:11:14 +0500

    Сделано первое задание

--- Program.cs
+++ Program.cs
@@ -14,7 +14,40 @@ namespace TwoRolesClassOOP
     {
         static void Main(string[] args)
         {
-            
+            BankOfAccount bankOfAccount = new();
+            bankOfAccount.Account();
+            bankOfAccount.Balance();
+            bankOfAccount.TypeAccount();
         }
+        class BankOfAccount
+        {
+            private int accountBank { get; set; }
+
+            private int balanceAccount { get; set; }
+
+            private enum typeBankAccount { current,estimated,credit }
+
+            public void Account()
+            {
+                
+                accountBank = 8689;
+                Console.WriteLine("Номер счета: " + accountBank);
+            }
+            public void Balance()
+            {
+                balanceAccount = 3000000;
+                Console.WriteLine("Баланс на счете: "+balanceAccount);
+            }
+            public void TypeAccount()
+            {
+                typeBankAccount type = typeBankAccount.estimated;
+                Console.WriteLine("Тип банковского счета: "+type);
+                
+            }
+        }
+
     }
 }
+/*Создать класс счет в банке с закрытыми полями: номер счета, баланс, тип банковского счета (использовать перечислимый тип).
+Предусмотреть методы для доступа к данным – заполнения и чтения. Создать объект класса, заполнить его поля и вывести
+информацию об объекте класса на печать*/
