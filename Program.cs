fa3731a00ed1173dd0afb194724c6490ef935f04
ALLUK <lam83@bk.ru>
2022-04-10 23:35:14 +0500

    FINAL The First task

--- Program.cs
+++ Program.cs
@@ -21,25 +21,27 @@ namespace TwoRolesClassOOP
         }
         class BankOfAccount
         {
-            private int AccountBank { get; set; }
+            private int _AccountBank { get; set; }
 
-            private int BalanceAccount { get; set; }
+            private int _BalanceAccount { get; set; }
 
             private enum TypeBankAccount { current,estimated,credit }
 
             public void Account()
             {
-                
-                AccountBank = 8689;
-                Console.WriteLine("Номер счета: " + AccountBank);
+                int accountBank = _AccountBank;
+                accountBank = 8689;
+                Console.WriteLine("Номер счета: " + accountBank);
             }
             public void Balance()
             {
-                BalanceAccount = 3000000;
-                Console.WriteLine("Баланс на счете: "+BalanceAccount);
+                int balanceAccount = _BalanceAccount;
+                balanceAccount = 3000000;
+                Console.WriteLine("Баланс на счете: "+balanceAccount);
             }
             public void TypeAccount()
             {
+                
                 TypeBankAccount type = TypeBankAccount.estimated;
                 Console.WriteLine("Тип банковского счета: "+type);
                 
@@ -48,7 +50,6 @@ namespace TwoRolesClassOOP
 
     }
 }
-//Создать класс счет в банке с закрытыми полями: номер счета, баланс, тип
-//банковского счета (использовать перечислимый тип). Предусмотреть методы для
-//доступа к данным – заполнения и чтения. Создать объект класса, заполнить его
-//поля и вывести информацию об объекте класса на печать
+//Создать класс счет в банке с закрытыми полями: номер счета, баланс, тип банковского счета (использовать перечислимый тип).
+//Предусмотреть методы для доступа к данным – заполнения и чтения.
+//Создать объект класса, заполнить его поля и вывести информацию об объекте класса на печать
